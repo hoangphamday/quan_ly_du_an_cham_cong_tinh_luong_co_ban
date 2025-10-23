f3d966d3bbcac00b10a3e511ab0d5c944e894f17
NgDucHieu13 <156333219+NgDucHieu13@users.noreply.github.com>
2025-10-23 08:58:54 +0700

    add bll duan and interfaces

---
+++ BLL/Duan_BLL.cs
@@ -0,0 +1,53 @@
+﻿using DAL;
+using Microsoft.IdentityModel.Tokens;
+using Models;
+using System;
+using Helper;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using BLL.Interfaces;
+using DAL.Interfaces;
+
+
+
+namespace BLL
+{
+    public partial class Duan_BLL : IDuan_BLL
+    {
+        private IDuAnRepository _res;
+        
+        public Duan_BLL (IDuAnRepository res)
+        {
+            _res = res;
+        }
+
+        public bool Create (DuAn duan)
+        {
+            return _res.Create (duan);
+        }
+
+        public bool Update(DuAn duan)
+        {
+            return _res.Update(duan);
+        }
+
+        public bool Delete(string id)
+        {
+            return _res.Delete(id);
+        }
+
+        public DuAn GetDuAnbyID(string id)
+        {
+            return _res.GetDuAnbyID(id);
+        }
+
+        public List<DuAn> Search(int pageIndex, int pageSize, out long total, string Tenduan, string Maduan)
+        {
+            return _res.Search(pageIndex, pageSize,out total, Tenduan, Maduan);
+        }
+    }
+}
---
+++ BLL/Interfaces/IDuan_BLL.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public partial interface IDuan_BLL
+    {
+        DuAn GetDuAnbyID(string id);
+        bool Create(DuAn duan);
+        bool Update(DuAn duan);
+        bool Delete(string id);
+        List<DuAn> Search(int pageIndex, int pageSize, out long total, string Tenduan, string Maduan);
+    }
+}
